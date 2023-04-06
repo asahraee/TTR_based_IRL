77a3798a7b6348f0f3f2ebe2a728a54a10d3f165
Atefeh <asahraee@sfu.ca>
2023-04-06 23:35:08 +0000

    latest changes from training docker

--- NN/datasets.py
+++ NN/datasets.py
@@ -14,15 +14,16 @@ import matplotlib.pyplot as plt
 class Dubin3D_A(Dataset):
     # This class loads ground truth local maps with relative x,y and initial theta as inputs, and ttr as output
     def __init__(self, textfile, labels_file, image_dir,
-            image_transform=None, target_transform_threshold=None):
+            target_transform_threshold=None, normalize=False, state_mean=None,
+            state_std= None, image_mean=None, image_std=None):
 
-        #pos_ttr = np.loadtxt(textfile, delimeter=',',
-        #        dtype=np.float32, skiprows=1)
         #threshold for geometric sseries to consider a value equal to infinity
         self._infin_tresh = target_transform_threshold
         r = torch.as_tensor(0.999)
         dt = torch.as_tensor(0.01)
         a = 1 * dt
+
+        # Read state and ttr from csv files
         pos_ttr = pd.read_csv(textfile, sep=',',
                 header=0, index_col=0).to_numpy()
         self._n_samples = pos_ttr.shape[0]
@@ -34,11 +35,10 @@ class Dubin3D_A(Dataset):
         #        axis=1).astype(np.float32)
         ttrs = pos_ttr[:,-1].astype(np.float32)
         
+        # Read image labels for indexing the image
         self._labels = pd.read_csv(labels_file, sep=',',
                 header=None, index_col =0)
         self._image_dir = image_dir
-        self._im_transform = image_transform
-        #self._vec_transform = vector_transform
         if self._infin_tresh:
             self._target_transform = trans.Lambda(lambda x: a*\
                     (1 - torch.pow(r, torch.floor(x/dt) + 1))/(1-r))
@@ -46,18 +46,46 @@ class Dubin3D_A(Dataset):
         self.states = torch.from_numpy(states)
         self.ttrs = torch.from_numpy(ttrs)
 
+        #self._im_transform = image_transform
+        if normalize:
+            # find state mean and standard deviation
+            if state_mean and state_std:
+                self._state_mean = state_mean
+                self._state_std = state_std
+            else:
+                self._df = pd.read_csv(textfile, index_col=0)
+                self._state_mean, self._state_std = self._compute_state_mean()
+            
+            ## find image mean and standard deviation
+            #if image_mean and image_std:
+            #    self._im_mean = image_mean
+            #    self._im_std = image_std
+            #else:
+            #    self._im_mean, self._im_std = self._compute_im_mean()
+            
+            # use mean and std to create normalization transform
+            #self._im_trans = trans.Normalize(self._im_mean, self._im_std)
+            self._im_trans = trans.Lambda(lambda im: im / 255)
+            self._state_trans = trans.Lambda(lambda st: \
+                    (st - self._state_mean)/self._state_std)
+        else:
+            self._im_trans = None
+            self._state_trans = None
+
     def __getitem__(self, index):
-        '''to be done'''
+        
+        # read image and apply transform
         image_path = os.path.join(self._image_dir,
                 self._labels.iloc[index, 0])
         image = read_image(image_path).float()
-        if self._im_transform:
-            image = self._im_transform(image)
-        
+        if self._im_trans:
+            image = self._im_trans(image)
+        # read state and apply transform
         state = self.states[index]
+        if self._state_trans:
+            state = self._state_trans(state)
+        # read ttr and apply transform
         ttr = self.ttrs[index]
-        #if self._vec_transform:
-        #    state = self._vec_transform(state)
         if self._infin_tresh:
             if ttr.item() <= self._infin_tresh: 
                 ttr = self._target_transform(ttr)
@@ -68,14 +96,28 @@ class Dubin3D_A(Dataset):
     def __len__(self):
         '''to be done'''
         return self._n_samples
+    
+    def _compute_state_mean(self):
+        df_mean = self._df.mean()
+        df_std = self._df.std()
+        pos_ttr_mean = [df_mean[key] for key in df_mean.keys()]
+        pos_ttr_std = [df_std[key] for key in df_std.keys()]
+        
+        state_mean = pos_ttr_mean[:-1]
+        state_std = pos_ttr_std[:-1]
+        
+        return torch.tensor(state_mean), torch.tensor(state_std)
+
+    def _compute_im_mean(self):
+        '''
+        To be done ...
+        '''
 
 class Dubin3D_B(Dataset):
     # This class loads ground truth global maps with relative x,y and initial theta as inputs and ttr as output
     def __init__(self, textfile, labels_file, image_dir,
             image_transform=None, target_transform_threshold=None):
 
-        #pos_ttr = np.loadtxt(textfile, delimeter=',',
-        #        dtype=np.float32, skiprows=1)
         #threshold for geometric sseries to consider a value equal to infinity
         self._infin_tresh = target_transform_threshold
         r = torch.as_tensor(0.999)
@@ -94,12 +136,7 @@ class Dubin3D_B(Dataset):
                 for l in self._labels.iloc[:,0]]
         self._image_dir = image_dir
         self._im_transform = image_transform
-        #self._vec_transform = vector_transform
         if self._infin_tresh:
-            #self._target_transform = trans.Lambda(lambda x:\
-            #        torch.div(\
-            #        torch.sub(torch.pow(r, torch.floor(torch.div(x, dt))), 1) ,\
-            #        torch.sub(r,1)))
             self._target_transform = trans.Lambda(lambda x: a*\
                     (1 - torch.pow(r, torch.floor(x/dt) + 1))/(1-r))
             self._target_trans_infin = a/(1-r)
@@ -115,8 +152,6 @@ class Dubin3D_B(Dataset):
 
         state = self.states[index]
         ttr = self.ttrs[index]
-        #if self._vec_transform:
-        #    state = self._vec_transform(state)
         if self._infin_tresh:
             if ttr.item() <= self._infin_tresh:
                 ttr = self._target_transform(ttr)
@@ -193,26 +228,21 @@ class Dubin3D_C(Dataset):
 
 
 def test():
-    data_path = '/root/Desktop/data_and_log/data_1'
+    data_path = '/root/Desktop/data_and_log/data_7'
     image_path = os.path.join(data_path, 'images/local/')
     text_file = os.path.join(data_path, 'csv/pos_ttr.csv')
     label_file = os.path.join(data_path, 'csv/image_labels.csv')
 
-    dataset = TrueLocMapDubin3D(text_file, label_file, image_path,
-            target_transform_threshold=1000)
+    dataset = Dubin3D_A(text_file, label_file, image_path,
+            target_transform_threshold=500, normalize=True)
     image_i, state_i, ttr_i = dataset[18]
-    print('state_i: ', state_i, 'ttr_i: ', ttr_i)
+    print('image_mean: ', image_i.mean(), ' image_std: ', image_i.std())
+    print('state_18: ', state_i, 'ttr_i: ', ttr_i)
     image_i, state_i, ttr_i = dataset[19]
-    print('state_i: ', state_i, 'ttr_i: ', ttr_i)
+    print('state_19: ', state_i, 'ttr_i: ', ttr_i)
     image_i, state_i, ttr_i = dataset[17]
-    print('state_i: ', state_i, 'ttr_i: ', ttr_i)
-    plt.imshow(image_i[0], cmap='gray')
-    plt.show()
+    print('state_17: ', state_i, 'ttr_i: ', ttr_i)
+    
 
-
-    dataset2 = TrueMapLidarRangeDubin3(text_file, label_file, image_path,
-            target_transform_threshold=1000)
-    image2i, state2i, rng2i, ttr2i = dataset2[10]
-    print('state2i: ', state2i, 'ttr2i: ', ttr2i, 'lidar2i', rng2i)
 
 if __name__=='__main__':test()
